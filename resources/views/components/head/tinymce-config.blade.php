07cd01335d504165af91204925b2802eb88f2590
Denny Setyawan Huda <denycam9@gmail.com>
2025-12-18 01:00:30 +0700

    chore: configure tinymce

---
+++ resources/views/components/head/tinymce-config.blade.php
@@ -0,0 +1,9 @@
+<script src="https://cdn.tiny.cloud/1/g6kyy0ynubaj9g91h43qmxvl8twjfmdni9dzxyqnpsgaymna/tinymce/8/tinymce.min.js"
+    referrerpolicy="origin" crossorigin="anonymous"></script>
+<script>
+    tinymce.init({
+        selector: 'textarea.tinymce',
+        plugins: 'code',
+        toolbar: 'undo redo | blocks | bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | code | table'
+    });
+</script>
